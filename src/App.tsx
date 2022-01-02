36e975093a19466f58990b6c979f30d47f7c6bb3
numeru <numeru@naver.com>
2022-01-02 19:30:47 +0900

    feat : 유저 상태 확인 api 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import { Redirect, Route, Switch } from 'react-router';
 import Home from '@pages/home';
 import Result from '@pages/result';
 import Layout from '@components/layouts';
 import SearchNewsService from '@services/search-news';
+import AuthService from '@apis/auth/auth-service';
+import { useSetRecoilState } from 'recoil';
+import { userState } from '@stores/user';
 
 type Props = {
 	searchNewsService: SearchNewsService;
+	authService: AuthService;
 };
 
-const App = ({ searchNewsService }: Props) => {
+const App = ({ searchNewsService, authService }: Props) => {
+	const [isUserChecked, setIsUserChecked] = useState(false);
+	const setUser = useSetRecoilState(userState);
+
+	const checkUserState = async () => {
+		setIsUserChecked(false);
+
+		try {
+			const result = await authService.getMyInfo();
+
+			if (result.data !== null) {
+				setUser((prev) => {
+					return {
+						...prev,
+						isLogin: true,
+						id: result.data!.userId,
+						keywords: result.data!.Keywords.slice(0, 5),
+					};
+				});
+			} else {
+				setUser((prev) => {
+					return {
+						...prev,
+						isLogin: false,
+						id: '',
+						keywords: [],
+					};
+				});
+			}
+		} catch (error) {
+			console.error(error);
+		}
+
+		setIsUserChecked(true);
+	};
+
+	useEffect(() => {
+		checkUserState();
+	}, []);
+
 	return (
-		<Layout>
-			<Switch>
-				<Route exact path="/">
-					<Home searchNewsService={searchNewsService} />
-				</Route>
-				<Route exact path="/result/:keyword">
-					<Result searchNewsService={searchNewsService} />
-				</Route>
-				<Redirect path="*" to="/" />
-			</Switch>
-		</Layout>
+		<>
+			{isUserChecked && (
+				<Layout authService={authService}>
+					<Switch>
+						<Route exact path="/">
+							<Home searchNewsService={searchNewsService} />
+						</Route>
+						<Route exact path="/result/:keyword">
+							<Result searchNewsService={searchNewsService} />
+						</Route>
+						<Redirect path="*" to="/" />
+					</Switch>
+				</Layout>
+			)}
+		</>
 	);
 };
 
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,9 +1,14 @@
 import { atom } from 'recoil';
 
-export const userState = atom({
+export const userState = atom<{
+	isLogin: boolean;
+	id: string;
+	keywords: string[];
+}>({
 	key: 'userState',
 	default: {
 		isLogin: false,
 		id: '',
+		keywords: [],
 	},
 });
