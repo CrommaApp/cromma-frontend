6051321beb488d012677e53dd214bc98edd4edea
numeru <numeru@naver.com>
2022-01-02 19:29:59 +0900

    conf : 프로젝트 이름 변경

--- webpack.config.ts
+++ webpack.config.ts
@@ -9,108 +9,105 @@ const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const dotenv = require('dotenv');
 
 interface Configuration extends WebpackConfiguration {
-  devServer?: WebpackDevServerConfiguration;
+	devServer?: WebpackDevServerConfiguration;
 }
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const config: Configuration = {
-  name: 'react-setup-without-cra',
-  mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'inline-source-map' : 'hidden-source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    alias: {
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@apis': path.resolve(__dirname, 'src/apis'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@stores': path.resolve(__dirname, 'src/stores'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@services': path.resolve(__dirname, 'src/services'),
-    },
-  },
-  entry: {
-    app: './src/index.tsx',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: { browsers: ['last 2 chrome versions'] },
-                debug: isDevelopment,
-              },
-            ],
-            '@babel/preset-react',
-            '@babel/preset-typescript',
-          ],
-          env: {
-            development: {
-              plugins: [
-                require.resolve('react-refresh/babel'),
-                'babel-plugin-styled-components',
-              ],
-            },
-          },
-        },
-        exclude: ['/node_modules'],
-      },
-      {
-        test: /\.css?$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-        options: {
-          name: '[hash].[ext]',
-          limit: 10000,
-        },
-      },
-    ],
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: isDevelopment ? 'development' : 'production',
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config().parsed),
-    }),
-    new WebpackManifestPlugin(),
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    publicPath: '/dist/',
-    clean: true,
-  },
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    compress: true,
-  },
+	name: 'cromma',
+	mode: isDevelopment ? 'development' : 'production',
+	devtool: isDevelopment ? 'inline-source-map' : 'hidden-source-map',
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+		alias: {
+			'@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@apis': path.resolve(__dirname, 'src/apis'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
+			'@stores': path.resolve(__dirname, 'src/stores'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@services': path.resolve(__dirname, 'src/services'),
+		},
+	},
+	entry: {
+		app: './src/index.tsx',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						[
+							'@babel/preset-env',
+							{
+								targets: { browsers: ['last 2 chrome versions'] },
+								debug: isDevelopment,
+							},
+						],
+						'@babel/preset-react',
+						'@babel/preset-typescript',
+					],
+					env: {
+						development: {
+							plugins: [require.resolve('react-refresh/babel'), 'babel-plugin-styled-components'],
+						},
+					},
+				},
+				exclude: ['/node_modules'],
+			},
+			{
+				test: /\.css?$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader',
+				options: {
+					name: '[hash].[ext]',
+					limit: 10000,
+				},
+			},
+		],
+	},
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			NODE_ENV: isDevelopment ? 'development' : 'production',
+		}),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new ForkTsCheckerWebpackPlugin({
+			async: false,
+		}),
+		new webpack.DefinePlugin({
+			'process.env': JSON.stringify(dotenv.config().parsed),
+		}),
+		new WebpackManifestPlugin(),
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js',
+		publicPath: '/dist/',
+		clean: true,
+	},
+	devServer: {
+		historyApiFallback: true,
+		port: 3000,
+		open: true,
+		compress: true,
+	},
 };
 
 if (isDevelopment && config.plugins) {
-  config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  config.plugins.push(new ReactRefreshWebpackPlugin());
+	config.plugins.push(new webpack.HotModuleReplacementPlugin());
+	config.plugins.push(new ReactRefreshWebpackPlugin());
 }
 if (!isDevelopment && config.plugins) {
-  config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
+	config.plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
 }
 
 export default config;
