5268012d7801e2ec77c263d8d031d760db1be7e3
numeru <numeru@naver.com>
2022-01-05 01:05:55 +0900

    fix : basename 제거

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const authService = new AuthService();
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter basename={process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : ''}>
+		<BrowserRouter>
 			<RecoilRoot>
 				<App searchNewsService={searchNewsService} authService={authService} />
 			</RecoilRoot>
