d8633cb4a1382878532fbe9e1a1af8220385796f
numeru <numeru@naver.com>
2022-01-08 21:35:08 +0900

    feat : 게시글 api 요청 클래스 추가

--- src/services/post/post-service.ts
+++ src/services/post/post-service.ts
@@ -1,8 +1,55 @@
+import { GetAllPostsResult, UploadPostRequest, UploadPostResult, getPostRequest, deletePostRequest } from './types';
 import axios, { AxiosInstance } from 'axios';
 import API_URL, { API_HOST, BasicResult } from '@services/api';
+import { getPostResult } from '@services/auth/types';
 
-class SearchNewsService {
-	constructor() {}
+class PostService {
+	private base: AxiosInstance;
+	private postUrl;
+
+	constructor() {
+		this.base = axios.create({
+			baseURL: API_HOST,
+			withCredentials: true,
+		});
+		this.postUrl = API_URL.post;
+	}
+
+	async getAllPosts() {
+		const { posts } = this.postUrl;
+
+		const response = await this.base.get(posts);
+		const result: GetAllPostsResult = await response.data;
+
+		return result;
+	}
+
+	async getPost({ postId }: getPostRequest) {
+		const { post } = this.postUrl;
+
+		const response = await this.base.get(`${post}/${postId}`);
+		const result: getPostResult = await response.data;
+
+		return result;
+	}
+
+	async uploadPost(data: UploadPostRequest) {
+		const { post } = this.postUrl;
+
+		const response = await this.base.post(post, data);
+		const result: UploadPostResult = await response.data;
+
+		return result;
+	}
+
+	async deletePost({ postId }: deletePostRequest) {
+		const { post } = this.postUrl;
+
+		const response = await this.base.delete(`${post}/${postId}`);
+		const result: BasicResult = await response.data;
+
+		return result;
+	}
 }
 
-export default SearchNewsService;
+export default PostService;
--- src/services/post/types.ts
+++ src/services/post/types.ts
@@ -1,6 +1,30 @@
+import { BasicResult } from '@services/api';
+
 export type Post = {
 	id: number;
 	title: string;
 	content: string;
 	createdAt: string;
+	updatedAt: string;
+};
+
+export type GetAllPostsResult = BasicResult & {
+	data: Post[] & { UserId: string };
+};
+
+export type UploadPostRequest = {
+	title: string;
+	content: string;
+};
+
+export type UploadPostResult = BasicResult & {
+	data: Post;
+};
+
+export type getPostRequest = {
+	postId: string;
+};
+
+export type deletePostRequest = {
+	postId: string;
 };
