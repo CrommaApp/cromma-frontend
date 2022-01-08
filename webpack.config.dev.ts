86c82f339b34499a7c7ae080def835a00472a138
numeru <numeru@naver.com>
2022-01-08 19:11:47 +0900

    fix : 절대 경로 수정

--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -14,7 +14,6 @@ const config = {
 		alias: {
 			'@hooks': path.resolve(__dirname, 'src/hooks'),
 			'@components': path.resolve(__dirname, 'src/components'),
-			'@apis': path.resolve(__dirname, 'src/apis'),
 			'@pages': path.resolve(__dirname, 'src/pages'),
 			'@utils': path.resolve(__dirname, 'src/utils'),
 			'@stores': path.resolve(__dirname, 'src/stores'),
--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -16,7 +16,6 @@ const config = {
 		alias: {
 			'@hooks': path.resolve(__dirname, 'src/hooks'),
 			'@components': path.resolve(__dirname, 'src/components'),
-			'@apis': path.resolve(__dirname, 'src/apis'),
 			'@pages': path.resolve(__dirname, 'src/pages'),
 			'@utils': path.resolve(__dirname, 'src/utils'),
 			'@stores': path.resolve(__dirname, 'src/stores'),
