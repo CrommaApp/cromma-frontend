18b809571710c450ce6ef5bf9efbedf628cdeaa9
numeru <numeru@naver.com>
2022-01-08 21:43:56 +0900

    feat : 게시글 조회 요청 커스텀 훅 적용

---
+++ src/hooks/useGetPost.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import PostService from '@services/post/post-service';
+import { Post } from '@services/post/types';
+import { useHistory, useParams } from 'react-router';
+
+type Params = {
+	postId: string;
+};
+
+type ReturnTypes = [Post, () => Promise<void>];
+
+const postService = new PostService();
+
+const useGetPost = (): ReturnTypes => {
+	const params = useParams<Params>();
+
+	const [post, setPost] = useState<Post>({
+		id: 1,
+		title: '',
+		content: '',
+		createdAt: '',
+		updatedAt: '',
+	});
+
+	const [postId, setPostId] = useState(params?.postId || '');
+
+	useEffect(() => {
+		if (params) {
+			setPostId(params.postId);
+		}
+	}, [params]);
+
+	const history = useHistory();
+
+	const getPost = async () => {
+		try {
+			const requestData = {
+				postId,
+			};
+
+			const { data, statusCode } = await postService.getPost(requestData);
+
+			if (statusCode === 200 && data) {
+				const curPost = {
+					...data,
+					createdAt: data.createdAt.slice(0, 10),
+				};
+				setPost(curPost);
+			}
+		} catch (error: any) {
+			if (error.response.data.statusCode === 404) {
+				alert(error.response.data.message);
+				history.replace('/');
+			}
+		}
+	};
+
+	return [post, getPost];
+};
+
+export default useGetPost;
--- src/pages/post/detail/index.tsx
+++ src/pages/post/detail/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Post } from '@services/post/types';
+import React, { useEffect } from 'react';
 import PostDetailContent from '@components/features/detail/detail-content';
 import { ContentWrapper } from '@components/shared/content-wrapper/styled';
+import useGetPost from '@hooks/useGetPost';
 
 const PostDetail = () => {
-	const mockPost: Post = {
-		id: 1,
-		title: 'title',
-		createdAt: '2021-12-12',
-		content: 'testetstetststsetseteste',
-	};
+	const [post, getPost] = useGetPost();
+
+	useEffect(() => {
+		getPost();
+	}, []);
+
 	return (
 		<>
 			<ContentWrapper>
 				<h1 className="a11y-hidden">게시글 전체 내용</h1>
-				<PostDetailContent post={mockPost} />
+				<PostDetailContent post={post} />
 			</ContentWrapper>
 		</>
 	);
