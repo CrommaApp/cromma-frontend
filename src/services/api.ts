9cff12d085ce30e03089a80937e5631992d72304
numeru <numeru@naver.com>
2022-01-15 19:41:51 +0900

    fix : api 경로 수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ export type BasicResult = {
 	message: string;
 };
 
-export const API_HOST = 'http://api.cromma.site';
+export const API_HOST = process.env.NODE_ENV !== 'production' ? 'http://localhost' : 'http://api.cromma.site';
 
 const API_URL = {
 	user: {
