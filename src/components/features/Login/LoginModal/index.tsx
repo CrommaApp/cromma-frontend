2cc08bb418d8f1b6b5303fcc42ca057e6e60f5cd
numeru <numeru@naver.com>
2022-02-06 16:23:42 +0900

    fix : 모달창 안으로 포커스 고정

--- src/components/features/Login/LoginModal/index.tsx
+++ src/components/features/Login/LoginModal/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Modal from '@components/shared/Modal';
 import LoginForm from '../LoginForm';
+import styled from 'styled-components';
+
+const CloseLoginModalButton = styled.button`
+	background-color: transparent;
+	font-size: 0.8rem;
+	color: #aaaaaa;
+	position: absolute;
+	top: 2%;
+	right: 2%;
+`;
 
 type Props = {
 	closeLoginModal: () => void;
 };
 
 const LoginModal = ({ closeLoginModal }: Props) => {
+	const preventTabMove = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+		if (!e.shiftKey && e.key === 'Tab') {
+			e.preventDefault();
+		}
+	};
 	return (
 		<Modal onCloseModal={closeLoginModal}>
 			<h1>Cromma</h1>
@@ -14,6 +29,9 @@ const LoginModal = ({ closeLoginModal }: Props) => {
 				로그인
 			</h2>
 			<LoginForm closeLoginModal={closeLoginModal} />
+			<CloseLoginModalButton type="button" onClick={closeLoginModal} onKeyDown={preventTabMove}>
+				닫기
+			</CloseLoginModalButton>
 		</Modal>
 	);
 };
